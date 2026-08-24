3241305b51eadd5aaade979c101945a926e21565
danchan22 <daniel.chancellor@gmail.com>
2026-08-24 13:42:32 -0400

    Update supabaseAdmin.ts

--- lib/supabaseAdmin.ts
+++ lib/supabaseAdmin.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabaseAdmin = createClient(
-  process.env.https://ncleeeywbznpacxxhqhd.supabase.co!,
-  process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5jbGVlZXl3YnpucGFjeHhocWhkIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4NzU4MTc2NCwiZXhwIjoyMTAzMTU3NzY0fQ.ZFK7A3Lq9z9ss4IWk4OBMGci5a7TnH6uqAciuzwHDIQ!
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
 );
